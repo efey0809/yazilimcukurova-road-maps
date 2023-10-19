24468f221ed8216441163932e0806c201a921dc3
Emrullah Yarar <emrullahyarar@outlook.com.tr>
2023-10-20 01:02:44 +0300

    random generator improved. now it generates random numbers and than sorting them into two different text file

--- test.c
+++ test.c
@@ -3,39 +3,71 @@
 #include <time.h>
 
 FILE * fptr;
+FILE * fptr2;
 
-struct randomNums{
-    int a;
+struct sayilar{
+    int x;
 };
 
 int main(){
 	
-	int ctr,a;
+	int inner, outer, temp, didSwap, a, b, ctr, nums[9999];
 	time_t t;
 	srand(time(&t));
 	
-	struct randomNums random[100];
+	struct sayilar sy[100];
 
 	for(ctr=0; ctr<100; ctr++){
-		random[ctr].a=rand()%1000+1;
+		sy[ctr].x=rand()%1000+1;
 	}
-	
+
 	fptr=fopen("/home/emrullah/Documents/random.txt", "w");
 
-	if(fptr==0){
-		printf("Error! File could not be opened.\n");
-		exit(1);
-	}
-	
-	fprintf(fptr, "Here is the random numbers: \n");
-	
-	for(ctr=0; ctr<100; ctr++){
-		fprintf(fptr, "%d\n", random[ctr].a);
-	}
+        if(fptr==0){
+                printf("Error! File could not be opened.\n");
+                exit(1);
+        }
 
-	fclose(fptr);
+        fprintf(fptr, "Here is the random numbers: \n");
+
+        for(ctr=0; ctr<100; ctr++){
+                fprintf(fptr, "%d\n", sy[ctr].x);
+        }
 	
 	printf("\n\tRandom numbers genareted\n");
+	
+	b=ctr;
+
+        for(outer=0; outer<ctr; outer++){
+            didSwap=0;
+            for(inner=outer; inner<b; inner++){
+                if(sy[inner].x<sy[outer].x){
+                    temp = sy[inner].x;
+                    sy[inner].x = sy[outer].x;
+                    sy[outer].x = temp;
+                    didSwap = 1;
+                }
+            }
+            if(didSwap == 0){
+                break;
+
+            }
+        }
+
+	fptr2=fopen("/home/emrullah/Documents/sortedrandom.txt", "w");
+
+        if(fptr2==0){
+                printf("Error! File could not be opened.\n");
+                exit(1);
+        }
+
+        fprintf(fptr2, "Here is the sorted random numbers: \n");
+
+        for(ctr=0; ctr<100; ctr++){
+                fprintf(fptr2, "%d\n", sy[ctr].x);
+        }
+
+        printf("\n\tRandom numbers sorted\n");
 
 	return 0;
 }

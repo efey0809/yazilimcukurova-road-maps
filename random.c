1f3d9f85be8992d359b98f334b71f08ab10b8e4d
Emrullah Yarar <emrullahyarar@outlook.com.tr>
2023-10-20 19:40:10 +0300

    f close function added

--- random.c
+++ random.c
@@ -35,6 +35,8 @@ int main(){
         }
 	
 	printf("\n\tRandom numbers genareted\n");
+
+	fclose(fptr);
 	
 	b=ctr;
 
@@ -67,7 +69,9 @@ int main(){
                 fprintf(fptr2, "%d\n", sy[ctr].x);
         }
 
-        printf("\n\tRandom numbers sorted\n");
+	printf("\n\tRandom numbers sorted\n");
+
+	fclose(fptr2);
 
 	return 0;
 }
